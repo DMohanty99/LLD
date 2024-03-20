1d6f9f0cebaad47e7755a28f57b02d034f660dae
dineshmohanty <dineshmohanty1999@gmail.com>
2024-03-21 01:06:31 +0530

    added the Singleton Design Pattern

---
+++ DesignPatterns/Singleton.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <bits/stdc++.h>
+class DICoreMain{
+    DICoreMain(){
+
+    }
+    
+    DICoreMain(DICoreMain&) =delete;
+    DICoreMain(DICoreMain&&) =delete;
+    DICoreMain operator=(DICoreMain&) =delete;
+    DICoreMain operator=(DICoreMain&&)= delete;
+
+    public:
+    static DICoreMain& getInstance(){
+        static DICoreMain m_inst;
+        return m_inst;
+    }
+    void doSomething(){
+        std::cout<<"it does something";
+    }
+};
+
+int main(){
+
+    DICoreMain::getInstance().doSomething();
+    return 0;
+}
